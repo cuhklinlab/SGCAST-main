eb83bd174ace3a9107755cc9a62e0464f432da44
LI Jinzhao <86294005+Robert9799@users.noreply.github.com>
2023-02-01 17:05:02 +0800

    Update main.py

--- GitHub_files/main.py
+++ GitHub_files/main.py
@@ -9,11 +9,6 @@ import copy
 from torch.utils.tensorboard import SummaryWriter
 
 def main():
-    # hardware constraint for speed test
-    # torch.set_num_threads(1)
-    #
-    # os.environ['OMP_NUM_THREADS'] = '1'
-
     # initialization
     config = Config()
 
@@ -23,11 +18,7 @@ def main():
             os.makedirs(path)
         writer = SummaryWriter(path)
         config_used = copy.copy(config)
-        # print('config_used.spot_paths first: ', config_used.spot_paths)
-        # print('config.spot_paths first : ', config.spot_paths)
         config_used.spot_paths = config.spot_paths[i]
-        # print('config_used.spot_paths: ', config_used.spot_paths)
-        # print('config.spot_paths first : ', config.spot_paths)
         torch.manual_seed(config_used.seed)
         random.seed(config_used.seed)
         np.random.seed(config_used.seed)
@@ -41,13 +32,8 @@ def main():
         for epoch in range(config_used.epochs_stage):
             print('Epoch:', epoch)
             model_train.train(epoch)
-            # print('loss', model_train.loss),print('loss', type(model_train.loss))
-            # print('lr', model_train.actual_lr), print('lr', type(model_train.actual_lr))
             writer.add_scalar('Loss/train', model_train.loss, epoch)
             writer.add_scalar('lr', model_train.actual_lr, epoch)
-            # loss = model_train.train(epoch)
-            # if epoch > 0 and loss < 0.14:
-            #     break
 
         b = datetime.now()
         print('End time: ', b.strftime('%H:%M:%S'))
@@ -60,34 +46,7 @@ def main():
         print('Training finished: ', datetime.now().strftime('%H:%M:%S'))
         print("torch.cuda.max_memory_allocated: %fGB" % (torch.cuda.max_memory_allocated(0) / 1024 / 1024 / 1024))
 
-    # torch.manual_seed(config.seed)
-    # random.seed(config.seed)
-    # np.random.seed(config.seed)
-    # a = datetime.now()
-    # print('Start time: ', a.strftime('%H:%M:%S'))
-    #
-    # # stage1 training
-    # torch.cuda.reset_peak_memory_stats()
-    # print('Training start ')
-    # model_train = Training(config)
-    # for epoch in range(config.epochs_stage):
-    #     print('Epoch:', epoch)
-    #     model_train.train(epoch)
-    #     # loss = model_train.train(epoch)
-    #     # if epoch > 0 and loss < 0.14:
-    #     #     break
-    #
-    # b = datetime.now()
-    # print('End time: ', b.strftime('%H:%M:%S'))
-    # c = b-a
-    # minutes = divmod(c.seconds, 60)
-    # print('Time used: ', minutes[0], 'minutes', minutes[1], 'seconds')
-    #
-    # print('Write embeddings')
-    # model_train.write_embeddings()
-    # print('Training finished: ', datetime.now().strftime('%H:%M:%S'))
-    # print("torch.cuda.max_memory_allocated: %fGB" % (torch.cuda.max_memory_allocated(0) / 1024 / 1024 / 1024))
-
+    
 
 
 
