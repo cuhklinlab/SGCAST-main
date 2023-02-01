8110fcc1d8151747a667c35b61c59713a3327786
LI Jinzhao <86294005+Robert9799@users.noreply.github.com>
2023-02-01 17:20:37 +0800

    Update train.py

--- GitHub_files/train.py
+++ GitHub_files/train.py
@@ -23,20 +23,15 @@ class Training():
         # initialize dataset
         self.model = SGCAST(config.nfeat, config.nhid, config.nemb).cuda() #torch.nn.DataParallel()
         self.actual_lr = 0.1
-        # initialize criterion (loss)
-        # self.criterion_cell = CellLoss()
-        # self.criterion_encoding = EncodingLoss(dim=64, p=config.p)
-        # self.l1_regular = L1regularization()
 
-        # initialize optimizer (sgd/momemtum/weight decay)
+        # initialize optimizer 
         self.optimizer = optim.SGD(self.model.parameters(), lr=self.config.lr_start,
-                                           # momentum=self.config.momentum,
-                                           weight_decay=0.) #SGD Adam
+                                           weight_decay=0.)
 
 
     def adjust_learning_rate(self, optimizer, epoch):
         if ((epoch - 0) // self.config.lr_decay_epoch) < self.config.lr_times:
-            self.actual_lr = self.config.lr_start * (.5 ** ((epoch - 0) // self.config.lr_decay_epoch)) #0.1 1.5
+            self.actual_lr = self.config.lr_start * (.5 ** ((epoch - 0) // self.config.lr_decay_epoch)) 
         else:
             self.actual_lr = self.actual_lr
         if (epoch - 0) % self.config.lr_decay_epoch == 0:
@@ -58,24 +53,8 @@ class Training():
         total_loss = 0.
         self.adjust_learning_rate(self.optimizer, epoch)
         self.loss = 0.
-
-        # initialize iterator
-        # iter_spot_loaders = []
-        # for spot_loader in self.train_spot_loaders:
-        #     iter_spot_loaders.append(cycle(iter(spot_loader)))
-        #     print("iter_spot_loaders type:", type(iter_spot_loaders))
-
-        ############### this part still gets problems!!!! loss in each iter_spot_loader?
         batch_idx = 0
         for data in self.train_loader:
-            # spot forward
-
-            # spot_cell_predictions = []
-            # for iter_spot_loader in iter_spot_loaders:
-            #     spot_data = next(iter_spot_loader)
-            #     # print("spot_data type:", type(spot_data))
-            #     # print("spot_data shape:", spot_data.shape)
-            #     # print("spot_data dtype:", spot_data.dtype)
 
             spot_data = data.float()
             coor = spot_data[:, -2:]
@@ -83,60 +62,33 @@ class Training():
             pdm_exp = torch.cdist(spot_data[:, :-2], spot_data[:, :-2]).cuda()
 
             n = pdm_exp.shape[0]
-            # self.exp = n * 2 * 960 # 2000
-            conexp_ratio =self.config.train_conexp_ratio #0.06 embryo  0.05 rest high-res
+            conexp_ratio =self.config.train_conexp_ratio
             conspa_ratio = self.config.train_conspa_ratio
 
-            # self.spa = n * 2 * 63
             res_exp = pdm_exp.masked_select(~torch.eye(n, dtype=bool).cuda())
             resexp_mean = torch.mean(res_exp)
             resexp_var =  torch.var(res_exp)
             res_spa = pdm_spa.masked_select(~torch.eye(n, dtype=bool).cuda())
             resspa_mean = torch.mean(res_spa)
             resspa_var =  torch.var(res_spa)
-            # print("mean df before:", resspa_mean - resexp_mean)
-            pdm_exp = pdm_exp * math.sqrt(resspa_var/resexp_var)  + resspa_mean - resexp_mean#* math.sqrt(resspa_var/resexp_var) #11.9
-            # print("rt_Sd:", math.sqrt(resspa_var/resexp_var))
+            pdm_exp = pdm_exp * math.sqrt(resspa_var/resexp_var)  + resspa_mean - resexp_mean
             res_exp = pdm_exp.masked_select(~torch.eye(n, dtype=bool).cuda())
-            # print("quantile:",self.exp/n/(n-1))
-            key_exp = torch.quantile(res_exp,conexp_ratio,interpolation="nearest") #self.exp/n/(n-1)  'higher'
+            key_exp = torch.quantile(res_exp,conexp_ratio,interpolation="nearest") 
             lexp = key_exp/math.sqrt((2*104))
-            # print("keyexp:", key_exp)
-            key_spa = torch.quantile(res_spa, conspa_ratio,interpolation="nearest") #self.spa / n / (n - 1)  'higher'
-            # print("keyspa:", key_spa)
+            key_spa = torch.quantile(res_spa, conspa_ratio,interpolation="nearest") 
             lspa = key_spa /math.sqrt((2 * 104))
-            # print("lexp:", lexp)
-            # print("lspa:", lspa)
-            # print("var ratio:", math.sqrt(resspa_var/resexp_var))
-            # res_exp = pdm_exp.masked_select(~torch.eye(n, dtype=bool).cuda())
-            # resexp_mean = torch.mean(res_exp);
-            # print("mean df after:", resspa_mean - resexp_mean)
-            # pdm_exp = pdm_exp + resspa_mean - resexp_mean
-            #
+
             pdm_exp = pdm_exp.fill_diagonal_(0)
             pdm_spa = pdm_spa.fill_diagonal_(0)
-            # print("spot device:", spot_data.device)
-            # print("x_pixel shape:", x_pixel.shape)
-                # prepare data
-                # spot_data = prepare_input([spot_data])
 
             spot_data = spot_data.cuda()
-            # spot_data = torch.permute(spot_data, (1, 0, 2))
-            # print("spot_data shape:", spot_data.shape)
-            # print("spot_data device:", spot_data.device)
-                # print("spot_data dtype:", spot_data.dtype)
-                # model forward
-            _, _, loss = self.model(spot_data[:, :-2], pdm_exp, pdm_spa, lexp, lspa)# pdm) #
+            _, _, loss = self.model(spot_data[:, :-2], pdm_exp, pdm_spa, lexp, lspa)
             del pdm_spa; del pdm_exp; del spot_data
-            # del coor; del pdm; del spot_data
-
-            # update encoding weights
             self.optimizer.zero_grad()
-            loss.backward() #retain_graph=True
-            clipping_value = 1  # arbitrary value of your choosing
+            loss.backward() 
+            clipping_value = 1  
             torch.nn.utils.clip_grad_norm_(self.model.parameters(), clipping_value)
             self.optimizer.step()
-            # print log
             total_loss += loss.data.item()
 
 
@@ -151,110 +103,52 @@ class Training():
             'model_encoding_state_dict': self.model.state_dict(),
             'optimizer': self.optimizer.state_dict()
         })
-        # if  (total_loss / (batch_idx + 1)) < 0.15:
-        #     break
-        # return (total_loss / batch_idx)
+
 
     def write_embeddings(self):
         self.model.eval()
         if not os.path.exists("output/"):
             os.makedirs("output/")
-
-        # spot data
-        # for i, spot_loader in enumerate(self.test_spot_loaders):
-        #     data_name = "stereo" #os.path.basename(self.config.spot_paths[i]).split('.')[0]
-        #     fp_em = open('./output/' + data_name + '_embeddings.txt', 'w')
         data_name = os.path.basename(self.config.spot_paths).split('.')[0]
         print("data name:", data_name)
 
         fp_em = open('./output/' + data_name + '_embeddings.txt', 'w')
-
-
         print("ready to write embeddings")
-        # torch.save(x , 'x-file') ; x = torch.load('x-file')
+
         batch_idx = 0
         for data in self.test_loader:
-            # spot_data = data.float().unsqueeze(0)
 
-            # for batch_idx, spot_data in enumerate(spot_loader):
-                # prepare data
-                # print("spot_data type:", type(spot_data))
-                # spot_data = prepare_input([spot_data])
-
-
-            # coor = spot_data[:, :, -2:].squeeze(0)
-            # pdm = torch.cdist(coor, coor).cuda()
-            # x_pixel = (spot_data[:, :, -2].data.cpu().numpy() )*100
-            # y_pixel = (spot_data[:, :, -1].data.cpu().numpy())*100
-            # print("x_pixel shape:", x_pixel.shape)
-            # prepare data
-            # spot_data = prepare_input([spot_data])
-
-            # spot_data = spot_data.cuda()
             spot_data = data.float()
             coor = spot_data[:, -2:]
             pdm_spa = torch.cdist(coor, coor).cuda()
             pdm_exp = torch.cdist(spot_data[:, :-2], spot_data[:, :-2]).cuda()
 
             n = pdm_exp.shape[0]
-            # self.exp = n * 2 * 960 # 2000
-            conexp_ratio = self.config.test_conexp_ratio #0.06 embryo  0.05 rest high-res
-            conspa_ratio = self.config.test_conspa_ratio # 0.06  0.05
-            # print("conspa_ratio:", conspa_ratio)
-            # self.spa = n * 2 * 63 # 64,65,66,67
+
+            conexp_ratio = self.config.test_conexp_ratio 
+            conspa_ratio = self.config.test_conspa_ratio 
             res_exp = pdm_exp.masked_select(~torch.eye(n, dtype=bool).cuda())
             resexp_mean = torch.mean(res_exp)
             resexp_var = torch.var(res_exp)
             res_spa = pdm_spa.masked_select(~torch.eye(n, dtype=bool).cuda())
             resspa_mean = torch.mean(res_spa)
             resspa_var = torch.var(res_spa)
-            pdm_exp = pdm_exp * math.sqrt(resspa_var / resexp_var) + resspa_mean - resexp_mean#* math.sqrt(resspa_var/resexp_var)
+            pdm_exp = pdm_exp * math.sqrt(resspa_var / resexp_var) + resspa_mean - resexp_mean
             res_exp = pdm_exp.masked_select(~torch.eye(n, dtype=bool).cuda())
-            key_exp = torch.quantile(res_exp,conexp_ratio,interpolation="nearest") #self.exp/n/(n-1) higher
+            key_exp = torch.quantile(res_exp,conexp_ratio,interpolation="nearest") 
             lexp = key_exp/math.sqrt((2*104))
-            # print("keyexp:", key_exp)
-            key_spa = torch.quantile(res_spa, conspa_ratio,interpolation="nearest") #self.spa / n / (n - 1) higher
-            # print("keyspa:", key_spa)
+            key_spa = torch.quantile(res_spa, conspa_ratio,interpolation="nearest") 
             lspa = key_spa /math.sqrt((2 * 104))
-            # print("lspa:", lspa)
 
-
-            # res_exp = pdm_exp.masked_select(~torch.eye(n, dtype=bool).cuda())
-            # resexp_mean = torch.mean(res_exp);
-            # print("mean df after:", resspa_mean - resexp_mean)
-            # pdm_exp = pdm_exp + resspa_mean - resexp_mean
-            # print("var ratio:", math.sqrt(resspa_var / resexp_var))
-            # print("mean df:", resspa_mean - resexp_mean)
             pdm_exp = pdm_exp.fill_diagonal_(0)
             pdm_spa = pdm_spa.fill_diagonal_(0)
 
-            # print("spot_data shape:", spot_data.shape)
-            # print("spot_data device:", spot_data.device)
-            # print("spot_data dtype:", spot_data.dtype)
+
             spot_data = spot_data.cuda()
             # model forward
             spot_embedding, _, _ = self.model(spot_data[:, :-2], pdm_exp, pdm_spa, lexp, lspa)# pdm)  #
             del pdm_spa; del pdm_exp; del spot_data
-            # del coor; del pdm; del spot_data
-
-            # x_pixel = spot_data[:, :, -2].data.cpu().numpy() - self.config.xstart
-            # y_pixel = spot_data[:, :, -1].data.cpu().numpy() - self.config.ystart
-            #     # prepare data
-            #     # spot_data = prepare_input([spot_data])
-            #
-            # spot_data = spot_data.cuda()
-            # # model forward
-            # _, _, spot_embedding, _ = self.model(spot_data, x_pixel, y_pixel, mask_ratio=0)
-            #
-            # del x_pixel;del y_pixel;del spot_data
-
             spot_embedding = torch.squeeze(spot_embedding, 0).data.cpu().numpy()
-
-
-
-            # normalization & softmax
-            # spot_embedding = spot_embedding / norm(spot_embedding, axis=1, keepdims=True)
-
 
             # write embeddings
             test_num, embedding_size = spot_embedding.shape[0], spot_embedding.shape[1]
