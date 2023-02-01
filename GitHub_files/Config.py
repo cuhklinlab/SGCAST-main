e5406701a524f43639f5e2bf797a750eb476147d
LI Jinzhao <86294005+Robert9799@users.noreply.github.com>
2023-02-01 17:40:31 +0800

    Update Config.py

--- GitHub_files/Config.py
+++ GitHub_files/Config.py
@@ -4,110 +4,79 @@ import os
 
 class Config(object):
     def __init__(self):
-        DB = 'stereo'
-        self.use_cuda = True
+        DT = 'DFPLC'  # 'stereo_mouse_bulb', 'seq-scope', 'stereo_embryo' change Data type before running.
         self.threads = 1
 
-        if not self.use_cuda:
-            self.device = torch.device('cpu')
+
+        if self.use_cuda:
+            self.device = torch.device('cuda:0')
         else:
             self.device = torch.device('cuda:0')
 
-        if DB == 'stereo':
-            # DB info
-            self.spot_paths = ["/lustre/project/Stat/s1155077016/spatial_data/151507/151507.h5ad",
-             "/lustre/project/Stat/s1155077016/spatial_data/151508/151508.h5ad",
-             "/lustre/project/Stat/s1155077016/spatial_data/151509/151509.h5ad",
-             "/lustre/project/Stat/s1155077016/spatial_data/151510/151510.h5ad",
-             "/lustre/project/Stat/s1155077016/spatial_data/151669/151669.h5ad",
-             "/lustre/project/Stat/s1155077016/spatial_data/151670/151670.h5ad",
-             "/lustre/project/Stat/s1155077016/spatial_data/151671/151671.h5ad",
-             "/lustre/project/Stat/s1155077016/spatial_data/151672/151672.h5ad",
-             "/lustre/project/Stat/s1155077016/spatial_data/151673/151673.h5ad",
-             "/lustre/project/Stat/s1155077016/spatial_data/151674/151674.h5ad",
-             "/lustre/project/Stat/s1155077016/spatial_data/151675/151675.h5ad",
-             "/lustre/project/Stat/s1155077016/spatial_data/151676/151676.h5ad"]
-
-            # ["/lustre/project/Stat/s1155077016/spatial_data/Stereo-seq/Stereo-seq.h5ad",
-            #  '/lustre/project/Stat/s1155077016/spatial_data/scope_colon/scope_colon.h5ad',
-            #  '/lustre/project/Stat/s1155077016/spatial_data/scope_colon_2112_SW4X/scope_colon_2112_SW4X.h5ad',
-            #  '/lustre/project/Stat/s1155077016/spatial_data/stereo_E16/E16_SS200000124BR_D1D2_bin50.h5ad']
-            # ['/lustre/project/Stat/s1155077016/spatial_data/stereo_E16/E16_SS200000124BR_D1D2_bin50.h5ad']
-
-            # ["/lustre/project/Stat/s1155077016/spatial_data/Stereo-seq/Stereo-seq.h5ad",
-            # "/lustre/project/Stat/s1155077016/spatial_data/slidev2_mouse_OlfactoryBulb/slidev2_mouse_OlfactoryBulb.h5ad"]
-
-            # ['/lustre/project/Stat/s1155077016/spatial_data/scope_colon/scope_colon.h5ad',
-            #  '/lustre/project/Stat/s1155077016/spatial_data/scope_liverTD/scope_liverTD.h5ad',
-            #  '/lustre/project/Stat/s1155077016/spatial_data/scope_liver/scope_liver.h5ad',
-            #  '/lustre/project/Stat/s1155077016/spatial_data/scope_colon_2112_SW4X/scope_colon_2112_SW4X.h5ad']
-
-            # ["/lustre/project/Stat/s1155077016/spatial_data/151507/151507.h5ad",
-            #  "/lustre/project/Stat/s1155077016/spatial_data/151508/151508.h5ad",
-            #  "/lustre/project/Stat/s1155077016/spatial_data/151509/151509.h5ad",
-            #  "/lustre/project/Stat/s1155077016/spatial_data/151510/151510.h5ad",
-            #  "/lustre/project/Stat/s1155077016/spatial_data/151669/151669.h5ad",
-            #  "/lustre/project/Stat/s1155077016/spatial_data/151670/151670.h5ad",
-            #  "/lustre/project/Stat/s1155077016/spatial_data/151671/151671.h5ad",
-            #  "/lustre/project/Stat/s1155077016/spatial_data/151672/151672.h5ad",
-            #  "/lustre/project/Stat/s1155077016/spatial_data/151673/151673.h5ad",
-            #  "/lustre/project/Stat/s1155077016/spatial_data/151674/151674.h5ad",
-            #  "/lustre/project/Stat/s1155077016/spatial_data/151675/151675.h5ad",
-            #  "/lustre/project/Stat/s1155077016/spatial_data/151676/151676.h5ad"]
-            # ['/lustre/project/Stat/s1155077016/spatial_data/Puck_200127_15/Puck_200127_15.h5ad',
-            #  '/lustre/project/Stat/s1155077016/spatial_data/Puck_190921_21/Puck_190921_21.h5ad',
-            #  '/lustre/project/Stat/s1155077016/spatial_data/Puck_191204_01/Puck_191204_01.h5ad',
-            #  '/lustre/project/Stat/s1155077016/spatial_data/Puck_200115_08/Puck_200115_08.h5ad']
-            # ['/lustre/project/Stat/s1155077016/spatial_data/stereo_E9/E9_DP8400015286BL_D2_bin50.h5ad',
-            # '/lustre/project/Stat/s1155077016/spatial_data/stereo_E10/E10_FP200000587TR_E4_bin50.h5ad',
-            # '/lustre/project/Stat/s1155077016/spatial_data/stereo_E11/E11_FP200000554BL_B2_bin50.h5ad',
-            # '/lustre/project/Stat/s1155077016/spatial_data/stereo_E12/E12_FP200000587TR_D5_bin50.h5ad',
-            # '/lustre/project/Stat/s1155077016/spatial_data/stereo_E13/E13_SS200000124BR_C3C4_bin50.h5ad',
-            # '/lustre/project/Stat/s1155077016/spatial_data/stereo_E14/E14_SS200000108BR_C3C4_bin50.h5ad',
-            #
-            # '/lustre/project/Stat/s1155077016/spatial_data/stereo_E15/E15_SS200000108BR_E1E2_bin50.h5ad',
-            # '/lustre/project/Stat/s1155077016/spatial_data/stereo_E16/E16_SS200000124BR_D1D2_bin50.h5ad']
-            # '/lustre/project/Stat/s1155077016/spatial_data/stereo_E16/stereo_E16.h5ad'
-            # '/lustre/project/Stat/s1155077016/spatial_data/Puck_200127_15/Puck_200127_15.h5ad'
-
-            # '/lustre/project/Stat/s1155077016/spatial_data/seq-scope_colon/scope_colon.h5ad'
-            # '/lustre/project/Stat/s1155077016/spatial_data/seq-scope_liverTD/scope_liverTD.h5ad'
-            # '/lustre/project/Stat/s1155077016/spatial_data/seq-scope_liver/scope_liver.h5ad'
-            # 'Puck_200127_15' 'Puck_190921_21' 'Puck_191204_01' 'Puck_200115_08'
-            # '/lustre/project/Stat/s1155077016/spatial_data/Puck_200127_15/Puck_200127_15.h5ad'
-            # "/lustre/project/Stat/s1155077016/spatial_data/151676/151676.h5ad"
-             # "/lustre/project/Stat/s1155077016/spatial_data/Slide-seqV2_mouse_OlfactoryBulb/slidev2.h5ad"
-            # '/lustre/project/Stat/s1155077016/spatial_data/seq-scope/scope.h5ad'
-            # "/lustre/project/Stat/s1155077016/spatial_data/Stereo-seq/stereo.h5ad"
-            # "/lustre/project/Stat/s1155077016/spatial_data/151674/151674.h5ad"
+        if DT == 'DFPLC':
+            # DT info
+            self.spot_paths = ["/datapath/151507/151507.h5ad",
+             "/datapath/151508/151508.h5ad",
+             "/datapath/151509/151509.h5ad",
+             "/datapath/151510/151510.h5ad",
+             "/datapath/151669/151669.h5ad",
+             "/datapath/151670/151670.h5ad",
+             "/datapath/151671/151671.h5ad",
+             "/datapath/151672/151672.h5ad",
+             "/datapath/151673/151673.h5ad",
+             "/datapath/151674/151674.h5ad",
+             "/datapath/151675/151675.h5ad",
+             "/datapath/151676/151676.h5ad"]
+            
+            self.train_conexp_ratio = 0.07 
+            self.train_conspa_ratio = 0.07
+            self.test_conexp_ratio = 0.07 
+            self.test_conspa_ratio = 0.07 
+            
+          if DT == 'stereo_mouse_bulb': 
+            self.spot_paths = ["/lustre/project/Stat/s1155077016/spatial_data/Stereo-seq/Stereo-seq.h5ad"]
+            
+            self.train_conexp_ratio = 0.07 
+            self.train_conspa_ratio = 0.02
+            self.test_conexp_ratio = 0.07 
+            self.test_conspa_ratio = 0.07 
+            
+          if DT == 'seq-scope': 
+            self.spot_paths = ['/lustre/project/Stat/s1155077016/spatial_data/scope_colon/scope_colon.h5ad']
+            
+            self.train_conexp_ratio = 0.07
+            self.train_conspa_ratio = 0.04
+            self.test_conexp_ratio = 0.07 
+            self.test_conspa_ratio = 0.07 
+            
+          if DT == 'stereo_embryo': 
+            self.spot_paths = ['/datapath/stereo_E12/E12_FP200000587TR_D5_bin50.h5ad',
+             '/datapath/stereo_E14/E14_SS200000108BR_C3C4_bin50.h5ad',
+             '/datapath/stereo_E16/E16_SS200000124BR_D1D2_bin50.h5ad']
+            
+            self.train_conexp_ratio = 0.02 
+            self.train_conspa_ratio = 0.02
+            self.test_conexp_ratio = 0.06 
+            self.test_conspa_ratio = 0.06
+        # For low-resolution, it is fine to set all $/tau$ to 0.07. For high-resolution data, when training, low $/tau$ is preferred for smaller loss and faster convergence
+        # when writing result, using common 0.07 is okay. For embryo data, especially E16.5, the spots number is too large, we pick a bit smaller $/tau$ 0.06 .
+                     
+        # Training config
+        self.nfeat = 50 
+        self.nhid = 50
+        self.nemb = 50
+        self.batch_size = 2000  
+        self.lr_start = 0.2 
+        self.lr_times = 2
+        self.lr_decay_epoch = 80 
+        self.epochs_stage =100 
+        self.momentum = 0.9
+        self.seed = 2022
+        self.checkpoint = ''
 
 
-            # Training config
-            self.nfeat = 50 #30
-            self.nhid = 50
-            self.nemb = 50
-            # self.exp = self.batch_size*1 # 2000
-            # self.spa = self.batch_size*2*6   #200000
-            # self.init = "louvain"
-            self.batch_size = 2000  #### 2000
-            self.lr_start = 0.2 #0.12
-            self.lr_times = 2
-            self.lr_decay_epoch = 80 #500
-            self.epochs_stage =100 #2000
-            # self.embedding_size = 64
-            self.momentum = 0.9
-            self.seed = 2022
-            self.checkpoint = ''
-            self.train_conexp_ratio = 0.07 #0.02 embryo
-            self.train_conspa_ratio = 0.07#0.02 embryo
-            self.test_conexp_ratio = 0.07 #0.06 embryo
-            self.test_conspa_ratio = 0.07 #0.06 embryo
-            # self.exp_nei = 0.25 #0.05
-            # self.spa_nei = 0.064 #0.05
 
 
-            # lr_time =1
-            # different lr schedule
 
 
 
