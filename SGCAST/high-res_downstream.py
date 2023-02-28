2fbaf22723f6d25d29ca32cb5f2f6d3549bb6947
LI Jinzhao <86294005+Robert9799@users.noreply.github.com>
2023-02-28 17:14:09 +0800

    Update high-res_downstream.py

--- SGCAST/high-res_downstream.py
+++ SGCAST/high-res_downstream.py
@@ -1,3 +1,5 @@
+# This file is used to do clustering, refinement and testing of differentially expressed genes given embeddings from high-resolution data.
+
 import scanpy as sc
 import numpy as np
 import pandas as pd
@@ -14,12 +16,11 @@ file_name = "/datapath/"+ID+"/"+ID+".h5ad"
 spots_embeddings = np.loadtxt(os.path.join(base_path, ID + '_embeddings.txt'))
 stage = ID
 adata = sc.read_h5ad(file_name)
-adata.obsm['embedding'] = np.float32(spots_embeddings)
-n_neighbors = 30 # common setting: 9, 15, 25, 30
+adata.obsm['embedding'] = np.float32(spots_embeddings) # save memory
+n_neighbors = 30 # common setting: 9, 15, 25, 30 
 sc.pp.neighbors(adata, use_rep='embedding', n_neighbors=n_neighbors) 
 resolution = 1.2  
 sc.tl.louvain(adata,  resolution=resolution)  
-# del spots_embeddings
 size = 6
 figsize = (3, 3) 
 plt.rcParams["figure.figsize"] = figsize
@@ -28,7 +29,7 @@ sc.pl.embedding(adata, basis="spatial", color="louvain", s=size, show=False,
 ###########refine
 sc.pp.neighbors(adata, use_rep='spatial', n_neighbors=n_neighbors, key_added="pixel")
 arr = adata.obsp['pixel_connectivities']
-arr = arr.astype('int32')
+arr = arr.astype('int32') # save memory
 pred = adata.obs["louvain"].astype('int32')
 refined_pred = refine_high(pred=np.array(pred.tolist()), dis=arr, option=False)
 adata.obs["refined_pred"] = refined_pred
@@ -44,6 +45,7 @@ plt.close()
 
 num_cluster = len(np.unique(adata.obs["refined_pred"]))
 
+# find differentially expressed genes given above clusters.
 if 'log1p' in list(adata.uns):
     del adata.uns['log1p']
 
@@ -51,11 +53,14 @@ if 'log1p' in list(adata.uns):
 adata.obs["refined_pred"]=adata.obs["refined_pred"].astype(str)
 sc.tl.rank_genes_groups(adata, 'refined_pred', method='wilcoxon', key_added="wilcoxon")  
 
+# generate dotplot
 sc.pl.rank_genes_groups_dotplot(adata, n_genes=5, key="wilcoxon", groupby='refined_pred',save=ID+'dotplot.png')
+
 DEs = []
 df = sc.get.rank_genes_groups_df(adata, key='wilcoxon', group=None)
 groups = df['group'].unique()
 
+# select top 2 genes for each identified cluster
 for group in groups:
     dfg = df.loc[df['group'] == group]
     names = dfg['names'][:2].tolist()
@@ -64,11 +69,13 @@ for group in groups:
         
 print(ID + '_DEs are:', DEs)
 
+# flip and rotate the plot
 adata.obsm["spatial"] = adata.obsm["spatial"][:, [1, 0]]
 adata.obsm["spatial"][:, 0] = adata.obsm["spatial"][:, 0]*(-1)
 
 adata.obsm["spatial"] = adata.obsm["spatial"][:, [1, 0]]
 
+# plot the expression of each DE
 for DE in DEs:
     plot_gene = DE
     spot_size = 20
