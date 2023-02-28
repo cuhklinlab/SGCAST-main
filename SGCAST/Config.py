6be74097870013e6b3914d4a643bf821a7ec60ef
LI Jinzhao <86294005+Robert9799@users.noreply.github.com>
2023-02-28 16:24:25 +0800

    Update Config.py

--- SGCAST/Config.py
+++ SGCAST/Config.py
@@ -10,18 +10,18 @@ class Config(object):
 
         if DT == 'DLPFC':
             # DT info
-            self.spot_paths = ["/lustre/project/Stat/s1155077016/spatial_data/151507/151507.h5ad",
-             "/lustre/project/Stat/s1155077016/spatial_data/151508/151508.h5ad",
-             "/lustre/project/Stat/s1155077016/spatial_data/151509/151509.h5ad",
-             "/lustre/project/Stat/s1155077016/spatial_data/151510/151510.h5ad",
-             "/lustre/project/Stat/s1155077016/spatial_data/151669/151669.h5ad",
-             "/lustre/project/Stat/s1155077016/spatial_data/151670/151670.h5ad",
-             "/lustre/project/Stat/s1155077016/spatial_data/151671/151671.h5ad",
-             "/lustre/project/Stat/s1155077016/spatial_data/151672/151672.h5ad",
-             "/lustre/project/Stat/s1155077016/spatial_data/151673/151673.h5ad",
-             "/lustre/project/Stat/s1155077016/spatial_data/151674/151674.h5ad",
-             "/lustre/project/Stat/s1155077016/spatial_data/151675/151675.h5ad",
-             "/lustre/project/Stat/s1155077016/spatial_data/151676/151676.h5ad"]
+            self.spot_paths = ["/datapath/151507.h5ad",
+             "/datapath/151508.h5ad",
+             "/datapath/151509.h5ad",
+             "/datapath/151510.h5ad",
+             "/datapath/151669.h5ad",
+             "/datapath/151670.h5ad",
+             "/datapath/151671.h5ad",
+             "/datapath/151672.h5ad",
+             "/datapath/151673.h5ad",
+             "/datapath/151674.h5ad",
+             "/datapath/151675.h5ad",
+             "/datapath/151676.h5ad"]
             
             self.train_conexp_ratio = 0.07 
             self.train_conspa_ratio = 0.07
@@ -31,7 +31,7 @@ class Config(object):
  
             
         if DT == 'seq-scope': 
-            self.spot_paths = ['/lustre/project/Stat/s1155077016/spatial_data/scope_colon/scope_colon.h5ad']
+            self.spot_paths = ['/datapath/scope_colon.h5ad']
             
             self.train_conexp_ratio = 0.07
             self.train_conspa_ratio = 0.04
@@ -39,7 +39,7 @@ class Config(object):
             self.test_conspa_ratio = 0.07 
             
         if DT == 'stereo_mouse_bulb': 
-            self.spot_paths = ["/lustre/project/Stat/s1155077016/spatial_data/Stereo-seq/Stereo-seq.h5ad"]
+            self.spot_paths = ["/datapath/Stereo-seq.h5ad"]
             
             self.train_conexp_ratio = 0.07
             self.train_conspa_ratio = 0.02
@@ -47,15 +47,15 @@ class Config(object):
             self.test_conspa_ratio = 0.07 
             
         if DT == 'stereo_embryo': 
-            self.spot_paths = ['/lustre/project/Stat/s1155077016/spatial_data/E12_FP200000587TR_D5_bin50.h5ad',
-             '/lustre/project/Stat/s1155077016/spatial_data/stereo_E14/E14_SS200000108BR_C3C4_bin50.h5ad',
-             '/lustre/project/Stat/s1155077016/spatial_data/stereo_E16/E16_SS200000124BR_D1D2_bin50.h5ad']
+            self.spot_paths = ['/datapath/E12_FP200000587TR_D5_bin50.h5ad',
+             '/datapath/E14_SS200000108BR_C3C4_bin50.h5ad',
+             '/datapath/E16_SS200000124BR_D1D2_bin50.h5ad']
             
             self.train_conexp_ratio = 0.02 
             self.train_conspa_ratio = 0.02
             self.test_conexp_ratio = 0.06 
             self.test_conspa_ratio = 0.06
-        # For low-resolution data, it is fine to set all $/tau$ to 0.07, which is default setting in SGCAST. 
+        # For low-resolution data, it is fine to set all $/tau$ to 0.07, which is the default setting in SGCAST. 
         # For high-resolution data, when training, low $/tau$ is preferred for smaller loss and faster convergence.
         # when writing results, using common 0.07 is okay. For embryo data, especially E16.5, the spots number is too large, we pick a bit smaller $/tau$ 0.06 .
                      
