83f3ae41d6651fedacb071e28a2d034a0a668d4c
LI Jinzhao <86294005+Robert9799@users.noreply.github.com>
2023-02-28 17:38:48 +0800

    Update train.py

--- SGCAST/train.py
+++ SGCAST/train.py
@@ -30,6 +30,7 @@ class Training():
 
 
     def adjust_learning_rate(self, optimizer, epoch):
+        # the function makes the learning rate 0.2 at first 80 epochs and decay to 0.1 at last 20 epochs
         if ((epoch - 0) // self.config.lr_decay_epoch) < self.config.lr_times:
             self.actual_lr = self.config.lr_start * (.5 ** ((epoch - 0) // self.config.lr_decay_epoch)) 
         else:
@@ -47,7 +48,8 @@ class Training():
                 self.model.load_state_dict(checkpoint['model_encoding_state_dict'])
             else:
                 print("=> no resume checkpoint found at '{}'".format(self.config.checkpoint))
-
+                
+    # train function
     def train(self, epoch):
         self.model.train()
         total_loss = 0.
@@ -55,37 +57,56 @@ class Training():
         self.loss = 0.
         batch_idx = 0
         for data in self.train_loader:
-
+            # load data for a mini-batch
             spot_data = data.float()
             coor = spot_data[:, -2:]
+            
+            # calculate pairwise distance matrix for spatial coordiantes and gene expression
             pdm_spa = torch.cdist(coor, coor).cuda()
             pdm_exp = torch.cdist(spot_data[:, :-2], spot_data[:, :-2]).cuda()
 
             n = pdm_exp.shape[0]
             conexp_ratio =self.config.train_conexp_ratio
             conspa_ratio = self.config.train_conspa_ratio
-
+            
+            # select non-diagonal elements 
             res_exp = pdm_exp.masked_select(~torch.eye(n, dtype=bool).cuda())
+            
+            # calculate mean and variance of distances
             resexp_mean = torch.mean(res_exp)
             resexp_var =  torch.var(res_exp)
             res_spa = pdm_spa.masked_select(~torch.eye(n, dtype=bool).cuda())
             resspa_mean = torch.mean(res_spa)
             resspa_var =  torch.var(res_spa)
+            
+            # rescale distances in gene expression
             pdm_exp = pdm_exp * math.sqrt(resspa_var/resexp_var)  + resspa_mean - resexp_mean
             res_exp = pdm_exp.masked_select(~torch.eye(n, dtype=bool).cuda())
+            
+            # find the quantile (conexp_ratio) of distances in expression layer
             key_exp = torch.quantile(res_exp,conexp_ratio,interpolation="nearest") 
-            lexp = key_exp/math.sqrt((2*104))  # torch.exp(-104) = 0 ; -104 is the m in paper.
+            
+            # determine l in expression layer for this mini-batch
+            lexp = key_exp/math.sqrt((2*104))  # torch.exp(-104) = 0 ; -104 is the largest integer.
+            
+            # find the quantile (conspa_ratio) of distances in spatial layer
             key_spa = torch.quantile(res_spa, conspa_ratio,interpolation="nearest") 
+            
+            # determine l in spatial layer for this mini-batch
             lspa = key_spa /math.sqrt((2 * 104))
-
+            
+            # the distance on diagonal line is 0 (the distance to spot itself)
             pdm_exp = pdm_exp.fill_diagonal_(0)
             pdm_spa = pdm_spa.fill_diagonal_(0)
-
+            
+            # move data to GPU
             spot_data = spot_data.cuda()
             _, _, loss = self.model(spot_data[:, :-2], pdm_exp, pdm_spa, lexp, lspa)
             del pdm_spa; del pdm_exp; del spot_data
             self.optimizer.zero_grad()
             loss.backward() 
+            
+            # clip gradient by 1
             clipping_value = 1  
             torch.nn.utils.clip_grad_norm_(self.model.parameters(), clipping_value)
             self.optimizer.step()
@@ -104,7 +125,7 @@ class Training():
             'optimizer': self.optimizer.state_dict()
         })
 
-
+    # write results after training
     def write_embeddings(self):
         self.model.eval()
         if not os.path.exists("output/"):
@@ -116,6 +137,7 @@ class Training():
         print("ready to write embeddings")
 
         batch_idx = 0
+        # same procedure as training but no backward loss to update parameter in the model.
         for data in self.test_loader:
 
             spot_data = data.float()
@@ -148,6 +170,8 @@ class Training():
             # model forward
             spot_embedding, _, _ = self.model(spot_data[:, :-2], pdm_exp, pdm_spa, lexp, lspa)# pdm)  #
             del pdm_spa; del pdm_exp; del spot_data
+            
+            # move results from GPU to CPU
             spot_embedding = torch.squeeze(spot_embedding, 0).data.cpu().numpy()
 
             # write embeddings
