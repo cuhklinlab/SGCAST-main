fd6ecbdc5e82d307410d5d0099770b30b904e801
LI Jinzhao <86294005+Robert9799@users.noreply.github.com>
2023-07-17 12:37:15 +0800

    Update Config_spa.py

--- SGCAST_spa/Config_spa.py
+++ SGCAST_spa/Config_spa.py
@@ -10,18 +10,18 @@ class Config(object):
 
         if DT == 'DLPFC':
             # DT info
-            self.spot_paths = ["/datapath/151507.h5ad",
-             "/datapath/151508.h5ad",
-             "/datapath/151509.h5ad",
-             "/datapath/151510.h5ad",
-             "/datapath/151669.h5ad",
-             "/datapath/151670.h5ad",
-             "/datapath/151671.h5ad",
-             "/datapath/151672.h5ad",
-             "/datapath/151673.h5ad",
-             "/datapath/151674.h5ad",
-             "/datapath/151675.h5ad",
-             "/datapath/151676.h5ad"]
+            self.spot_paths = ["../data/DLPFC/151507/151507.h5ad",
+             "../data/DLPFC/151508/151508.h5ad",
+             "../data/DLPFC/151509/151509.h5ad",
+             "../data/DLPFC/151510/151510.h5ad",
+             "../data/DLPFC/151669/151669.h5ad",
+             "../data/DLPFC/151670/151670.h5ad",
+             "../data/DLPFC/151671/151671.h5ad",
+             "../data/DLPFC/151672/151672.h5ad",
+             "../data/DLPFC/151673/151673.h5ad",
+             "../data/DLPFC/151674/151674.h5ad",
+             "../data/DLPFC/151675/151675.h5ad",
+             "../data/DLPFC/151676/151676.h5ad"]
             
             self.train_conexp_ratio = 0.07 
             self.train_conspa_ratio = 0.07
@@ -29,35 +29,6 @@ class Config(object):
             self.test_conspa_ratio = 0.07 
             
  
-            
-        if DT == 'seq-scope': 
-            self.spot_paths = ['/datapath/scope_colon.h5ad']
-            
-            self.train_conexp_ratio = 0.07
-            self.train_conspa_ratio = 0.04
-            self.test_conexp_ratio = 0.07 
-            self.test_conspa_ratio = 0.07 
-            
-        if DT == 'stereo_mouse_bulb': 
-            self.spot_paths = ["/datapath/Stereo-seq.h5ad"]
-            
-            self.train_conexp_ratio = 0.07
-            self.train_conspa_ratio = 0.02
-            self.test_conexp_ratio = 0.07 
-            self.test_conspa_ratio = 0.07 
-            
-        if DT == 'stereo_embryo': 
-            self.spot_paths = ['/datapath/E12_FP200000587TR_D5_bin50.h5ad',
-             '/datapath/E14_SS200000108BR_C3C4_bin50.h5ad',
-             '/datapath/E16_SS200000124BR_D1D2_bin50.h5ad']
-            
-            self.train_conexp_ratio = 0.02 
-            self.train_conspa_ratio = 0.02
-            self.test_conexp_ratio = 0.06 
-            self.test_conspa_ratio = 0.06
-        # For low-resolution data, it is fine to set all $/tau$ to 0.07, which is the default setting in SGCAST. 
-        # For high-resolution data, when training, low $/tau$ is preferred for smaller loss and faster convergence.
-        # when writing results, using common 0.07 is okay. For embryo data, especially E16.5, the spots number is too large, we pick a bit smaller $/tau$ 0.06 .
                      
         # Training config
         self.nfeat = 50 
